3bb0e45a4389144d20e5a9c0b6569eb5909894cf
Javier <153399346+javierIsaii@users.noreply.github.com>
2025-10-26 10:35:35 -0600

    Update animator.js

--- test/v4/animator.js
+++ test/v4/animator.js
@@ -23,17 +23,8 @@ document.addEventListener("DOMContentLoaded", function () {
     animateElements();
 });
 
-document.addEventListener("DOMContentLoaded", () => {
-  setTimeout(() => {
-    const elements = document.querySelectorAll(".welcomeLine, .anotherImg, .player-elements, .player-buttons, .card");
-    elements.forEach((el, index) => {
-      // Añadimos un pequeño delay incremental si querés efecto secuencial
-      setTimeout(() => {
-        el.classList.add("fade-in-up");
-      }, index * 250); // cada elemento entra con 150ms de diferencia
-    });
-  }, 1800); // espera 1s antes de iniciar las apariciones
-});
+
+
 
 
 
